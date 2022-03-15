5f6158dce5b9c47950768b2a99f2d0d179987c69
Pavol Hvolka <palo.hvolka@plus4u.net>
2022-03-15 19:34:47 +0100

    upgrade recipe

--- Users/pavol.hvolka/Documents/@programming/kucharka/models/recipe.js
+++ Users/pavol.hvolka/Documents/@programming/kucharka/models/recipe.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose')
 
 const recipeSchema = new mongoose.Schema({
     name: String,
+    subname: String,
     image: String,
-    describtion: String
+    time: Number,
+    difficulty: String,
+    describtion: String,
+    materials: Array
 })
 
 module.exports = mongoose.model('Recipe', recipeSchema)
